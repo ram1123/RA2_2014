fa3b26ee163fd3c445fed7cbf05518f3cbc2036a
Arne-Rasmus Draeger <adraeger@cern.ch>
2014-12-05 14:18:03 +0100

    First version New TreeMaker

--- AllHadronicSUSY/TreeMaker/interface/TreeMaker.h
+++ AllHadronicSUSY/TreeMaker/interface/TreeMaker.h
@@ -18,6 +18,8 @@
 #include "TTree.h"
 #include <TFile.h>
 #include <vector>
+#include "TLorentzVector.h"
+#include "Math/GenVector/LorentzVector.h"
 #include <DataFormats/Math/interface/deltaR.h>
 #include "DataFormats/PatCandidates/interface/PackedGenParticle.h"
 #include "DataFormats/Candidate/interface/Candidate.h"
@@ -42,14 +44,15 @@ private:
 	void setBranchVariablesToDefault();
 	TString treeName_;
 	TTree* tree_;	
+	bool debug_;
 	// generell event information
 	UInt_t runNum_;      
 	UInt_t lumiBlockNum_;
 	UInt_t evtNum_;
 	// any float precision varialbes
-//	std::vector<edm::InputTag> varsFloatTags_;
-//	std::vector<std::string> varsFloatNames_;
-//	std::vector<Float_t> varsFloat_;
+	std::vector<edm::InputTag> varsDoubleTags_;
+	std::vector<std::string> varsDoubleNames_;
+	std::vector<Float_t> varsDouble_;
 	// any int precision varialbes
 	std::vector<edm::InputTag> varsIntTags_;
 	std::vector<std::string> varsIntNames_;
@@ -58,6 +61,14 @@ private:
 	std::vector<edm::InputTag> varsBoolTags_;
 	std::vector<std::string> varsBoolNames_;
 	std::vector<UChar_t> varsBool_;
+	// any TLorentzVector precision varialbes
+	std::vector<edm::InputTag> varsTLorentzVectorTags_;
+	std::vector<std::string> varsTLorentzVectorNames_;
+	std::vector<TLorentzVector> varsTLorentzVector_;
+	// any TLorentzVector precision varialbes
+	std::vector<edm::InputTag> vectorTLorentzVectorTags_;
+	std::vector<std::string> vectorTLorentzVectorNames_;
+	std::vector<std::vector< TLorentzVector> > vectorTLorentzVector_;
 	// any reco candidate plus addiation doubles
 	std::vector<edm::InputTag> varsRecoCandTags_;
 	std::vector<std::string> varsRecoCandNames_;
@@ -66,7 +77,12 @@ private:
 	std::vector<Float_t*> RecoCandEta_;
 	std::vector<Float_t*> RecoCandPhi_;
 	std::vector<Float_t*> RecoCandE_;
-	std::vector<UShort_t> RecoCandAdditionalVariablesN_;
-	std::vector< std::vector<edm::InputTag> > RecoCandAdditionalVariablesTags_;
-	std::vector< std::vector<Float_t*> > RecoCandAdditionalVariables_;
+	std::vector<TLorentzVector*> RecoCandLorentzVector_;
+	std::vector<UShort_t> RecoCandAdditionalBoolVariablesN_,RecoCandAdditionalIntVariablesN_,RecoCandAdditionalFloatVariablesN_;
+	std::vector< std::vector<edm::InputTag> > RecoCandAdditionalBoolVariablesTags_;
+	std::vector< std::vector<UChar_t*> > RecoCandAdditionalBoolVariables_;
+	std::vector< std::vector<edm::InputTag> > RecoCandAdditionalIntVariablesTags_;
+	std::vector< std::vector<Int_t*> > RecoCandAdditionalIntVariables_;
+	std::vector< std::vector<edm::InputTag> > RecoCandAdditionalFloatVariablesTags_;
+	std::vector< std::vector<Float_t*> > RecoCandAdditionalFloatVariables_;
 };
--- AllHadronicSUSY/TreeMaker/src/TreeMaker.cc
+++ AllHadronicSUSY/TreeMaker/src/TreeMaker.cc
@@ -39,12 +39,17 @@ TreeMaker::TreeMaker(const edm::ParameterSet& iConfig)
 {
   // generell
   treeName_ = iConfig.getParameter<std::string>("TreeName");
-  // input tags for float variables eg HT MHT MET or what not
-  varsFloatNames_= iConfig.getParameter< std::vector<std::string> >  ("VarsFloat");
-  // input tags for int variables eg NJets nBJets etc.
-  varsIntNames_= iConfig.getParameter< std::vector<std::string> >  ("VarsInt");
-  // input tags for bool variables eg filters
-  varsBoolNames_= iConfig.getParameter< std::vector<std::string> >  ("VarsBool");
+	debug_ = iConfig.getParameter<bool> ("debug");
+	varsDoubleTags_ = iConfig.getParameter< std::vector<edm::InputTag> >("VarsDouble");
+	varsDoubleNames_= iConfig.getParameter< std::vector<std::string> >  ("VarsDoubleNamesInTree");
+	varsIntTags_ = iConfig.getParameter< std::vector<edm::InputTag> >("VarsInt");
+	varsIntNames_= iConfig.getParameter< std::vector<std::string> >  ("VarsIntNamesInTree");
+	varsBoolTags_ = iConfig.getParameter< std::vector<edm::InputTag> >("VarsBool");
+	varsBoolNames_= iConfig.getParameter< std::vector<std::string> >  ("VarsBoolNamesInTree");
+	varsTLorentzVectorTags_ = iConfig.getParameter< std::vector<edm::InputTag> >("VarsTLorentzVector");
+	varsTLorentzVectorNames_= iConfig.getParameter< std::vector<std::string> >  ("VarsTLorentzVectorNamesInTree");
+	vectorTLorentzVectorTags_ = iConfig.getParameter< std::vector<edm::InputTag> >("VectorTLorentzVector");
+	vectorTLorentzVectorNames_= iConfig.getParameter< std::vector<std::string> >  ("VectorTLorentzVectorNamesInTree");
   // input tags for float variables eg HT MHT MET or what not
   varsRecoCandNames_= iConfig.getParameter< std::vector<std::string> >  ("VarsRecoCand");
 }
@@ -68,8 +73,121 @@ TreeMaker::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
   using namespace reco;
   using namespace pat;
   setBranchVariablesToDefault();
-  
-
+	
+	// Event information
+	edm::EventAuxiliary aux = iEvent.eventAuxiliary();
+	runNum_       = aux.run();
+	lumiBlockNum_ = aux.luminosityBlock();
+	evtNum_       = aux.event();
+	//Float variables
+	for(unsigned int i = 0; i < varsDoubleTags_.size(); ++i) {
+		edm::Handle<double> var;
+		iEvent.getByLabel(varsDoubleTags_.at(i),var);
+		if( var.isValid() ) {
+			varsDouble_.at(i) = *var;
+		}
+	}
+	for(unsigned int i = 0; i < varsIntTags_.size(); ++i) {
+		edm::Handle<int> var;
+		iEvent.getByLabel(varsIntTags_.at(i),var);
+		if( var.isValid() ) {
+			varsInt_.at(i) = *var;
+		}
+	}
+	for(unsigned int i = 0; i < varsBoolTags_.size(); ++i) {
+		edm::Handle<bool> dec;
+		iEvent.getByLabel(varsBoolTags_.at(i),dec);
+		if( dec.isValid() ) {
+			if( *dec ) varsBool_.at(i) = 1;
+			else varsBool_.at(i) = 0;
+		}
+	}
+	for(unsigned int i = 0; i < varsTLorentzVectorTags_.size(); ++i) {
+		edm::Handle<TLorentzVector> var;
+		iEvent.getByLabel(varsTLorentzVectorTags_.at(i),var);
+		if( var.isValid() ) {
+			varsTLorentzVector_.at(i) = *var;
+		}
+	}
+	for(unsigned int i = 0; i < varsTLorentzVectorTags_.size(); ++i) {
+		edm::Handle<std::vector<TLorentzVector> > var;
+		iEvent.getByLabel(varsTLorentzVectorTags_.at(i),var);
+		if( var.isValid() ) 
+		{
+			for(unsigned int i=0; i< var->size();i++)
+			{
+				vectorTLorentzVector_.at(i).push_back(var->at(i));
+			}
+		}
+	}
+	for(unsigned int i = 0; i < varsRecoCandTags_.size(); ++i) 
+	{
+		edm::Handle< edm::View<reco::Candidate> > cands;
+		iEvent.getByLabel(varsRecoCandTags_.at(i),cands);
+		if( cands.isValid() ) 
+		{
+			RecoCandN_[i] = (unsigned int)(cands->size());
+			for(unsigned int ii=0; ii < cands->size();ii++)
+			{
+				RecoCandPt_.at(i)[ii] = cands->at(ii).pt();
+				RecoCandEta_.at(i)[ii] = cands->at(ii).eta();
+				RecoCandPhi_.at(i)[ii] = cands->at(ii).phi();
+				RecoCandE_.at(i)[ii] = cands->at(ii).energy();
+				RecoCandLorentzVector_.at(i)[ii].SetXYZT(cands->at(ii).p4().X(),cands->at(ii).p4().Y(),cands->at(ii).p4().Z(),cands->at(ii).p4().T());
+			}
+			// loop over double variables
+			for(unsigned int ii=0; ii< RecoCandAdditionalFloatVariablesTags_[i].size();ii++)
+			{
+				edm::Handle<std::vector<double> > FloatVar;
+				iEvent.getByLabel(RecoCandAdditionalFloatVariablesTags_[i].at(ii),FloatVar);
+				if( !FloatVar.isValid() )
+				{
+					if(debug_)std::cout<<"Warning Float variable with lable: "<<RecoCandAdditionalFloatVariablesTags_[i].at(ii).label()<<" not found!!!"<<std::endl;
+					break;
+				}
+				for(unsigned int iii=0; iii<cands->size();iii++)
+				{
+					RecoCandAdditionalFloatVariables_.at(i)[ii][iii] = FloatVar->at(iii);
+				}
+			}
+			// loop over int variables
+			for(unsigned int ii=0; ii< RecoCandAdditionalIntVariablesTags_[i].size();ii++)
+			{
+				edm::Handle<std::vector<int> > IntVar;
+				iEvent.getByLabel(RecoCandAdditionalIntVariablesTags_[i].at(ii),IntVar);
+				if( !IntVar.isValid() )
+				{
+					if(debug_)std::cout<<"Warning Int variable with lable: "<<RecoCandAdditionalIntVariablesTags_[i].at(ii).label()<<" not found!!!"<<std::endl;
+					break;
+				}
+				for(unsigned int iii=0; iii<cands->size();iii++)
+				{
+					RecoCandAdditionalIntVariables_.at(i)[ii][iii] = IntVar->at(iii);
+				}
+			}
+			// loop over bool variables
+			for(unsigned int ii=0; ii< RecoCandAdditionalBoolVariablesTags_[i].size();ii++)
+			{
+				edm::Handle<std::vector<bool> > boolVar;
+				iEvent.getByLabel(RecoCandAdditionalBoolVariablesTags_[i].at(ii),boolVar);
+				if( !boolVar.isValid() )
+				{
+					if(debug_)std::cout<<"Warning bool variable with lable: "<<RecoCandAdditionalBoolVariablesTags_[i].at(ii).label()<<" not found!!!"<<std::endl;
+					break;
+				}
+				for(unsigned int iii=0; iii<cands->size();iii++)
+				{
+					if( boolVar->at(iii) ) RecoCandAdditionalBoolVariables_.at(i)[ii][iii] = 1;
+					else RecoCandAdditionalBoolVariables_.at(i)[ii][iii] = 0;
+				}
+			}
+		}
+		else if(debug_)std::cout<<"Warning recoCand with tag: "<<varsRecoCandTags_.at(i).label()<<" not found!"<<std::endl;
+		
+	}
+	
+// std::cout<<"Filling tree with:"<<std::endl;
+// for(unsigned int i=0; i<RecoCandN_.size();i++)std::cout<<"RecoCand["<<i<<"] Numberofentries: "<<RecoCandN_.at(i)<<std::endl;
   tree_->Fill();
 }
 
@@ -84,50 +202,163 @@ TreeMaker::beginJob()
   tree_ = fs->make<TTree>(treeName_,treeName_);  
   tree_->SetAutoSave(10000000000);
   tree_->SetAutoFlush(1000000);
+	tree_->Branch("RunNum",&runNum_,"RunNum/i");
+	tree_->Branch("LumiBlockNum",&lumiBlockNum_,"LumiBlockNum/i");
+	tree_->Branch("EvtNum",&evtNum_,"EvtNum/i");
   // loop over input varsFloat string to extract optional names in tree
+	RecoCandN_ = std::vector<UShort_t>(varsRecoCandNames_.size(),0);
   for(unsigned int i=0; i<varsRecoCandNames_.size();i++)
   {
-    RecoCandN_.push_back(0);
     RecoCandPt_.push_back (new Float_t[200]);
     RecoCandEta_.push_back(new Float_t[200]);
     RecoCandPhi_.push_back(new Float_t[200]);
     RecoCandE_.push_back  (new Float_t[200]);
+		RecoCandLorentzVector_.push_back(new TLorentzVector[200]);
 
     std::string temp = varsRecoCandNames_[i];
+		std::string nameInTree = "";
+		std::string ttemp ="";
+		
 
-    std::cout<<"RecoCandNameFull["<<i<<"]: "<<temp<<std::endl;
+    std::cout<<"RecoCand Setup: item["<<i<<"] full string: "<<temp<<std::endl;
     if(temp.find('|')<temp.size() ) temp = temp.substr(0,temp.find("|") );
-    std::cout<<"Temp string stored in main inputTag "<<temp<<std::endl;
+		if(temp.find('(')<temp.size() && temp.find(')')<temp.size() ) 
+		{
+			nameInTree = temp.substr(temp.find('(')+1, temp.find(')')-temp.find('(')-1);
+			temp=temp.substr(0,temp.find('('));
+		}
+		else nameInTree = temp;
+    std::cout<<"RecoCand Tag: "<<temp<<std::endl;
     varsRecoCandTags_.push_back(edm::InputTag(temp));
-    tree_->Branch((temp+"Num").c_str(),&(RecoCandN_[i]),(temp+"Num/s").c_str());
+		std::cout<<"RecoCand stored name in tree: "<<nameInTree<<std::endl;
+		temp=nameInTree;
+    tree_->Branch((temp+"Num").c_str(),&(RecoCandN_.at(i)),(temp+"Num/s").c_str());
     tree_->Branch((temp+"Pt").c_str(), RecoCandPt_.at(i), (temp+"Pt["+temp+"Num]/F").c_str());
     tree_->Branch((temp+"Eta").c_str(),RecoCandEta_.at(i),(temp+"Eta["+temp+"Num]/F").c_str());
     tree_->Branch((temp+"Phi").c_str(),RecoCandPhi_.at(i),(temp+"Phi["+temp+"Num]/F").c_str());
     tree_->Branch((temp+"E").c_str(),  RecoCandE_.at(i),  (temp+"E["+temp+"Num]/F").c_str());
-    std::string mainTagName=temp;
+		tree_->Branch((temp+"TLorentzVector").c_str(),  RecoCandLorentzVector_.at(i),  (temp+"TLorentzVector["+temp+"Num]/F").c_str());
+		std::string mainNameInTree=temp;
     temp = varsRecoCandNames_[i];
-    unsigned int count=0;
-    std::vector<Float_t*> vec;
-    RecoCandAdditionalVariables_.push_back(vec);
+		unsigned int countBool=0;
+		unsigned int countInt=0;
+		unsigned int countFloat=0;
+		std::vector<UChar_t*> vecUChart;
+		RecoCandAdditionalBoolVariables_.push_back(vecUChart);
+		std::vector<Int_t*> vecInt;
+		RecoCandAdditionalIntVariables_.push_back(vecInt);
+    std::vector<Float_t*> vecFloat;
+		RecoCandAdditionalFloatVariables_.push_back(vecFloat);
     std::vector<edm::InputTag> tagvec;
-    RecoCandAdditionalVariablesTags_.push_back(tagvec);
+		RecoCandAdditionalBoolVariablesTags_.push_back(tagvec);
+		RecoCandAdditionalIntVariablesTags_.push_back(tagvec);
+		RecoCandAdditionalFloatVariablesTags_.push_back(tagvec);
     std::string temp2="";
+		std::string tag="";
+		int typ=-1;
     while (temp.find('|')<temp.size() ) // loop over the posible additonal variables
     {
-      RecoCandAdditionalVariables_[i].push_back(new Float_t[200]);
+			typ=-1;
       temp = temp.substr(temp.find("|")+1 );
       temp2="";
-      std::cout<<"| found in string: "<<temp<<std::endl;
       if(temp.find('|')<temp.size() ) temp2 = temp.substr(0, temp.find('|') );
       else temp2=temp;
-      std::cout<<"| TagName: "<<temp2<<std::endl;
-      RecoCandAdditionalVariablesTags_[i].push_back(edm::InputTag(temp2 ) );
-      tree_->Branch((mainTagName+"_"+temp2).c_str(), RecoCandAdditionalVariables_.at(i).at(count), (mainTagName+"_"+temp2+"["+mainTagName+"Num]/F").c_str());
-      count++;
+			// check for typ definition and for optional naming
+			if(temp2.find('(')<temp2.size() && temp2.find(')')<temp2.size() )
+			{
+			//	std::cout<<"POINT1::temp2: "<<temp2<<std::endl;
+				// check for optional naming
+				if(temp2.find('_')<temp2.size())
+				{
+					nameInTree = temp2.substr(temp2.find('_')+1, temp2.find(')')-temp2.find('_')-1);
+					tag = temp2.substr(0, temp2.find('('));
+					if(temp2.find("b_")<temp2.size() )typ = 0;
+					if(temp2.find("I_")<temp2.size() )typ = 1;
+					if(temp2.find("F_")<temp2.size() )typ = 2;
+				}
+				else 
+				{
+					nameInTree = temp2.substr(0, temp2.find('('));
+					tag=temp2.substr(0, temp2.find('('));
+					if(temp2.find('b')<temp2.size() )typ = 0;
+					if(temp2.find('I')<temp2.size() )typ = 1;
+					if(temp2.find('F')<temp2.size() )typ = 2;
+
+				}
+			}
+			else if(typ==-1)std::cout<<"Warning no typ selected for additonal input object: "<<temp2<<" of main varialbe: "<<nameInTree<<"Please use: tag(x_Name) with x=b,I,F (bool, int float) and optional Name for naming in the tree"<<std::endl;
+     // std::cout<<"| TagName: "<<temp2<<std::endl;
+			std::cout<<"Sub Typ: Tag: "<<tag<<", typ: "<<typ<< ", nameIn Tree: "<<nameInTree<<std::endl;
+			if(typ==0)
+			{
+				
+				RecoCandAdditionalBoolVariablesTags_[i].push_back(edm::InputTag(tag ) );
+				RecoCandAdditionalBoolVariables_[i].push_back(new UChar_t[200]);
+				tree_->Branch((mainNameInTree+"_"+nameInTree).c_str(), RecoCandAdditionalBoolVariables_.at(i).at(countBool), (mainNameInTree+"_"+nameInTree+"["+mainNameInTree+"Num]/b").c_str());
+				countBool++;
+			}
+			if(typ==1)
+			{
+				
+				RecoCandAdditionalIntVariablesTags_[i].push_back(edm::InputTag(tag ) );
+				RecoCandAdditionalIntVariables_[i].push_back(new Int_t[200]);
+				tree_->Branch((mainNameInTree+"_"+nameInTree).c_str(), RecoCandAdditionalIntVariables_.at(i).at(countInt), (mainNameInTree+"_"+nameInTree+"["+mainNameInTree+"Num]/I").c_str());
+				countInt++;
+			}
+			if(typ==2)
+			{
+				
+			RecoCandAdditionalFloatVariablesTags_[i].push_back(edm::InputTag(tag ) );
+			RecoCandAdditionalFloatVariables_[i].push_back(new Float_t[200]);
+			tree_->Branch((mainNameInTree+"_"+nameInTree).c_str(), RecoCandAdditionalFloatVariables_.at(i).at(countFloat), (mainNameInTree+"_"+nameInTree+"["+mainNameInTree+"Num]/F").c_str());
+      countFloat++;
+			}
+			if(typ>2)std::cout<<"Error typ is: "<<typ<<" which is not defined!!! check!"<<std::endl;
     }
-    RecoCandAdditionalVariablesN_.push_back(count);
+    RecoCandAdditionalBoolVariablesN_.push_back(countBool);
+		RecoCandAdditionalIntVariablesN_.push_back(countInt);
+		RecoCandAdditionalFloatVariablesN_.push_back(countFloat);
   }
-  
+  for(unsigned int i = 0; i < varsBoolTags_.size(); ++i) {
+		TString name = "Bool_";
+		name += varsBoolTags_.at(i).label();
+		if( varsBoolNames_.size() == varsDoubleTags_.size() ) {
+			name = "Bool_"+varsBoolNames_.at(i);
+		}
+		tree_->Branch(name,&(varsBool_.at(i)),name+"/b");
+	}
+	for(unsigned int i = 0; i < varsIntTags_.size(); ++i) {
+		TString name = "Int_" + varsIntTags_.at(i).label();
+		if( varsIntNames_.size() == varsIntTags_.size() ) {
+			name = "Int_" +varsIntNames_.at(i);
+		}
+		tree_->Branch(name,&(varsIntTags_.at(i)),name+"/I");
+	}
+	for(unsigned int i = 0; i < varsDouble_.size(); ++i) {
+		TString name = "Float_" + varsDoubleTags_.at(i).label();
+		if( varsDoubleNames_.size() == varsDoubleTags_.size() ) {
+			name = "Float_" +varsDoubleNames_.at(i);
+		}
+		tree_->Branch(name,&(varsDouble_.at(i)),name+"/F");
+ 	}
+ 	for(unsigned int i = 0; i < varsTLorentzVectorTags_.size(); ++i) {
+		TString name = "TLorentzVector_";
+		name += varsTLorentzVectorTags_.at(i).label();
+		if( varsTLorentzVectorTags_.size() == varsTLorentzVectorNames_.size() ) {
+			name = "TLorentzVector_"+varsTLorentzVectorNames_.at(i);
+		}
+		tree_->Branch(name,name,&(varsTLorentzVector_.at(i)));
+	}
+	for(unsigned int i=0; i< vectorTLorentzVectorTags_.size();i++)
+	{
+		TString name = "VectorTLorentzVector_";
+		name += vectorTLorentzVectorTags_.at(i).label();
+		if(vectorTLorentzVectorNames_.size() == vectorTLorentzVectorTags_.size())
+		{
+			name = "VectorTLorentzVector_"+vectorTLorentzVectorNames_.at(i);
+		}
+		tree_->Branch(name,name,&(vectorTLorentzVector_.at(i)));
+	}
 }
 
 // ------------ method called once each job just after ending the event loop  ------------
@@ -147,7 +378,51 @@ TreeMaker::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
 void 
 TreeMaker::setBranchVariablesToDefault() 
 {
-  // event information
+	// event information
+	runNum_=0;
+	lumiBlockNum_=0;
+	evtNum_=0;
+  for(unsigned int i = 0; i < varsRecoCandTags_.size(); ++i) 
+	{
+		RecoCandN_.at(i)=0;
+		for(unsigned int ii=0; ii< nMaxCandidates_; ii++)
+		{
+			RecoCandPt_.at(i)[ii]=-999.;
+			RecoCandEta_.at(i)[ii]=-999.;
+			RecoCandPhi_.at(i)[ii]=-999.;
+			RecoCandE_.at(i)[ii]=-999.;
+			RecoCandLorentzVector_.at(i)[ii]=TLorentzVector (0.,0.,0.,0.);
+		}
+		// loop over the additonal variables
+		for(unsigned int iii=0; iii< nMaxCandidates_; iii++)
+		{
+			for(unsigned int ii=0; ii < RecoCandAdditionalBoolVariablesTags_.at(i).size();ii++)
+			{
+				RecoCandAdditionalBoolVariables_.at(i)[ii][iii]=0;
+			}
+			for(unsigned int ii=0; ii < RecoCandAdditionalIntVariablesTags_.at(i).size();ii++)
+			{
+				RecoCandAdditionalIntVariables_.at(i)[ii][iii]=-9999;
+			}
+			for(unsigned int ii=0; ii < RecoCandAdditionalFloatVariablesTags_.at(i).size();ii++)
+			{
+				RecoCandAdditionalFloatVariables_.at(i)[ii][iii]=-9999.;
+			}
+		}
+	}
+	for(unsigned int i = 0; i < varsBool_.size(); ++i) {
+		varsBool_.at(i) = 0;
+	}
+	for(unsigned int i = 0; i < varsInt_.size(); ++i) {
+		varsInt_.at(i) = 9999;
+	}
+	for(unsigned int i = 0; i < varsDouble_.size(); ++i) {
+		varsDouble_.at(i) = 9999.;
+	}
+	for(unsigned int i=0; i < vectorTLorentzVectorTags_.size();i++)
+	{
+		vectorTLorentzVector_.at(i).clear();
+	}
 
   
 }
