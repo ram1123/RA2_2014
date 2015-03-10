3646dc640cad95a3a77d074c66d647b6c43a39bd
lbrianza <lucabrianza@gmail.com>
2015-03-10 11:21:51 +0100

    jec correction

--- AllHadronicSUSY/Utils/python/metdouble_cfi.py
+++ AllHadronicSUSY/Utils/python/metdouble_cfi.py
@@ -2,4 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 metdouble = cms.EDProducer('METDouble',
 METTag  = cms.InputTag("slimmedMETs"),
+corrMet = cms.bool(False),
+JetTag_               = cms.InputTag('slimmedJets'),
+RhoTag = cms.InputTag("fixedGridRhoFastjetAll"),
+jecPayloadNames      = cms.vstring("JEC/PHYS14_25_V2::All_L3Absolute_AK4PFchs.txt","JEC/PHYS14_25_V2::All_L2Relative_AK4PFchs.txt","JEC/PHYS14_25_V2::All_L1FastJet_AK4PFchs.txt"),
+MuTag = cms.InputTag("slimmedMuons"),
 )
