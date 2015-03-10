77146b53b930a291b7b18feb78da9a2ad3158cc9
lbrianza <lucabrianza@gmail.com>
2015-03-10 11:21:47 +0100

    jec correction

--- AllHadronicSUSY/Utils/python/jetproperties_cfi.py
+++ AllHadronicSUSY/Utils/python/jetproperties_cfi.py
@@ -2,5 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 jetproperties = cms.EDProducer('JetProperties',
 JetTag_               = cms.InputTag('slimmedJets'),
+RhoTag = cms.InputTag("fixedGridRhoFastjetAll"),
+jecPayloadNames      = cms.vstring("JEC/PHYS14_25_V2::All_L3Absolute_AK4PFchs.txt","JEC/PHYS14_25_V2::All_L2Relative_AK4PFchs.txt","JEC/PHYS14_25_V2::All_L1FastJet_AK4PFchs.txt"),
 #BTagInputTag	        = cms.string('combinedSecondaryVertexBJetTags'),
 )
