6ecc8a2d78ef44a92a7bc9ddc09f9c3a9278dbb4
lbrianza <lucabrianza@gmail.com>
2015-03-13 18:35:17 +0100

    update for crab

--- AllHadronicSUSY/TreeMaker/test/prod_flatTrees.py
+++ AllHadronicSUSY/TreeMaker/test/prod_flatTrees.py
@@ -2,19 +2,20 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 config.section_('General')
 config.General.transferOutputs = True
-#config.General.requestName = 'RSGraviton1000'
-#config.General.workArea = 'RSGraviton1000'
+config.General.requestName = 'RSGraviton1000'
+config.General.workArea = 'RSGraviton1000'
 config.section_('JobType')
-config.JobType.psetName = 'test/runMakeTreeFromMiniAOD_cfg.py'
+config.JobType.psetName = 'TreeMaker/test/runMakeTreeFromMiniAOD_cfg.py'
 config.JobType.pluginName = 'Analysis'
-config.JobType.pyCfgParams = ['global_tag=PHYS14_25_V1::All', 'MC=True']
+config.JobType.pyCfgParams = ['global_tag=PHYS14_25_V2::All', 'MC=True']
 config.JobType.inputFiles = ['PHYS14_25_V2_All_L1FastJet_AK8PFchs.txt','PHYS14_25_V2_All_L2Relative_AK8PFchs.txt','PHYS14_25_V2_All_L3Absolute_AK8PFchs.txt','PHYS14_25_V2_All_L1FastJet_AK4PFchs.txt','PHYS14_25_V2_All_L2Relative_AK4PFchs.txt','PHYS14_25_V2_All_L3Absolute_AK4PFchs.txt' ]
+config.JobType.allowNonProductionCMSSW = True
 config.section_('Data')
-#config.Data.inputDataset = '/RSGravitonToWW_kMpl01_M_1000_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM'
+config.Data.inputDataset = '/RSGravitonToWW_kMpl01_M_1000_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM'
 config.Data.unitsPerJob = 1
 config.Data.inputDBS = 'global' #'http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet'
 config.Data.splitting = 'FileBased'
-#config.Data.outLFN = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/RSGraviton1000/'
+config.Data.outLFN = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/RSGraviton1000/'
 config.section_('User')
 config.section_('Site')
 config.Site.storageSite = 'T2_CH_CERN'
