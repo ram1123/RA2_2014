dec14defcc1a0068796b45cf79f5d701b62091d5
lbrianza <lucabrianza@gmail.com>
2015-05-25 17:56:36 +0200

    porting to 74x

--- AllHadronicSUSY/Utils/python/jetproperties_cfi.py
+++ AllHadronicSUSY/Utils/python/jetproperties_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 jetproperties = cms.EDProducer('JetProperties',
 JetTag_               = cms.InputTag('slimmedJets'),
+doJEC = cms.bool(False),
 RhoTag = cms.InputTag("fixedGridRhoFastjetAll"),
 L1File = cms.string("PHYS14_25_V2_All_L1FastJet_AK4PFchs.txt"),
 L2File = cms.string("PHYS14_25_V2_All_L2Relative_AK4PFchs.txt"),
