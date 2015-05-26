e9398021b4fbb4389c010d7398fe293d0aeefece
lbrianza <lucabrianza@gmail.com>
2015-05-26 15:27:35 +0200

    rm old folder

--- AllHadronicSUSY/Utils/python/electron_cfi.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-electron = cms.EDProducer('Electron',
-VertexTag = cms.InputTag("offlineSlimmedPrimaryVertices"),
-EleTag = cms.InputTag("slimmedElectrons"),
-RhoTag = cms.InputTag("fixedGridRhoFastjetAll"),
-ConversionTag = cms.InputTag('reducedEgamma:reducedConversions'),
-BeamSpotTag = cms.InputTag('offlineBeamSpot'),
-)
