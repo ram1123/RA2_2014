cf4f3ad8266f8206ee0afd16f3910cba626043c1
lbrianza <lucabrianza@gmail.com>
2015-06-03 12:07:25 +0200

    crab python for 74x

--- TreeMaker/test/prod_singleFlatTrees.py
+++ TreeMaker/test/prod_singleFlatTrees.py
@@ -7,15 +7,15 @@ config.General.workArea = 'RSGraviton1000'
 config.section_('JobType')
 config.JobType.psetName = 'TreeMaker/test/runMakeTreeFromMiniAOD_cfg.py'
 config.JobType.pluginName = 'Analysis'
-config.JobType.pyCfgParams = ['global_tag=PHYS14_25_V2::All', 'MC=True']
-config.JobType.allowNonProductionCMSSW = True
+config.JobType.pyCfgParams = ['global_tag=MCRUN2_74_V9::All', 'MC=True']
+config.JobType.allowUndistributedCMSSW = True
 config.JobType.inputFiles = ['PHYS14_25_V2_All_L1FastJet_AK8PFchs.txt','PHYS14_25_V2_All_L2Relative_AK8PFchs.txt','PHYS14_25_V2_All_L3Absolute_AK8PFchs.txt','PHYS14_25_V2_All_L1FastJet_AK4PFchs.txt','PHYS14_25_V2_All_L2Relative_AK4PFchs.txt','PHYS14_25_V2_All_L3Absolute_AK4PFchs.txt' ]
 config.section_('Data')
-config.Data.inputDataset = '/RSGravitonToWW_kMpl01_M_1000_Tune4C_13TeV_pythia8/Phys14DR-PU20bx25_tsg_PHYS14_25_V1-v2/MINIAODSIM'
+config.Data.inputDataset = '/RSGravToWW_kMpl01_M-1000_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/MINIAODSIM'
 config.Data.unitsPerJob = 50
 config.Data.inputDBS = 'global' #'http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet'
 config.Data.splitting = 'LumiBased'
-config.Data.outLFN = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/RSGraviton1000/'
+config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/RSGraviton1000/'
 config.section_('User')
 config.section_('Site')
 config.Site.storageSite = 'T2_CH_CERN'
