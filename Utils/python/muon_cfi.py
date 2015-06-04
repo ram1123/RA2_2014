91e2c5651e6958b432a4c0233ccd42597d0ff654
lbrianza <lucabrianza@gmail.com>
2015-06-04 09:32:05 +0200

    add some preselction

--- Utils/python/muon_cfi.py
+++ Utils/python/muon_cfi.py
@@ -3,5 +3,6 @@ import FWCore.ParameterSet.Config as cms
 muon = cms.EDProducer('Muon',
 VertexTag = cms.InputTag("offlineSlimmedPrimaryVertices"),
 MuTag = cms.InputTag("slimmedMuons"),
+MinPt = cms.double(15),
 RhoTag = cms.InputTag("fixedGridRhoFastjetAll"),
 )
