39d9e4a3ad6cf11aa077812d79303002abc0c162
lbrianza <lucabrianza@gmail.com>
2015-06-04 09:31:57 +0200

    add some preselction

--- Utils/python/jetpropertiesAK8_cfi.py
+++ Utils/python/jetpropertiesAK8_cfi.py
@@ -2,6 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 jetpropertiesAK8 = cms.EDProducer('JetPropertiesAK8',
 JetTag_               = cms.InputTag('slimmedJetsAK8'),
+MinPt = cms.double(15),
 puppiJetTag_               = cms.InputTag('ak8PFJetsPuppi'),
 doJEC = cms.bool(False),
 RhoTag = cms.InputTag("fixedGridRhoFastjetAll"),
