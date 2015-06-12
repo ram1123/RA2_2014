a6f0947750fcc1c999d601cc533430958257756d
lbrianza <lucabrianza@gmail.com>
2015-06-12 16:59:38 +0200

    update for producing data with crab

---
+++ TreeMaker/test/prod_singleFlatTrees_DATA.py
@@ -0,0 +1,21 @@
+from WMCore.Configuration import Configuration
+config = Configuration()
+config.section_('General')
+config.General.transferOutputs = True
+config.General.requestName = 'data'
+config.General.workArea = 'data'
+config.section_('JobType')
+config.JobType.psetName = 'TreeMaker/test/runMakeTreeFromMiniAOD_cfg.py'
+config.JobType.pluginName = 'Analysis'
+config.JobType.pyCfgParams = ['global_tag=GR_R_74_V9A::All', 'MC=False']
+config.JobType.allowUndistributedCMSSW = True
+config.JobType.inputFiles = ['PHYS14_25_V2_All_L1FastJet_AK8PFchs.txt','PHYS14_25_V2_All_L2Relative_AK8PFchs.txt','PHYS14_25_V2_All_L3Absolute_AK8PFchs.txt','PHYS14_25_V2_All_L1FastJet_AK4PFchs.txt','PHYS14_25_V2_All_L2Relative_AK4PFchs.txt','PHYS14_25_V2_All_L3Absolute_AK4PFchs.txt' ]
+config.section_('Data')
+config.Data.inputDataset = '/Jet/lbrianza-crab_data_Jet_Run2015A-PromptReco-v1-e050d919924aa3d4cdb5e91105bf430f/USER'
+config.Data.unitsPerJob = 50
+config.Data.inputDBS = 'phys03' #'http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet'
+config.Data.splitting = 'LumiBased'
+config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple_data/'
+config.section_('User')
+config.section_('Site')
+config.Site.storageSite = 'T2_CH_CERN'
