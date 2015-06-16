ecb56e442b496e1c3b6f15e83be3d6558bb634be
lbrianza <lucabrianza@gmail.com>
2015-06-16 17:50:38 +0200

    update crab files for data

--- TreeMaker/test/prod_miniAOD_forData.py
+++ TreeMaker/test/prod_miniAOD_forData.py
@@ -4,8 +4,8 @@ config = Configuration()
 config.section_('General')
 config.General.transferLogs = True
 config.General.transferOutputs = True
-config.General.requestName = 'data_Jet_Run2015A-PromptReco-v1'
-config.General.workArea = 'data_Jet_Run2015A-PromptReco-v1'
+config.General.requestName = 'data_Commissioning_Run2015A-PromptReco-v1'
+config.General.workArea = 'data_Commissioning_Run2015A-PromptReco-v1'
 
 config.section_('JobType')
 config.JobType.psetName = 'miniAOD-prod_PAT.py'
@@ -15,12 +15,12 @@ config.JobType.allowUndistributedCMSSW = True
 #config.JobType.inputFiles = ['PHYS14_25_V2_All_L1FastJet_AK8PFchs.txt','PHYS14_25_V2_All_L2Relative_AK8PFchs.txt','PHYS14_25_V2_All_L3Absolute_AK8PFchs.txt','PHYS14_25_V2_All_L1FastJet_AK4PFchs.txt','PHYS14_25_V2_All_L2Relative_AK4PFchs.txt','PHYS14_25_V2_All_L3Absolute_AK4PFchs.txt' ]
 
 config.section_('Data')
-config.Data.inputDataset = '/Jet/Run2015A-PromptReco-v1/AOD'
+config.Data.inputDataset = '/Commissioning/Run2015A-PromptReco-v1/AOD'
 config.Data.unitsPerJob = 50
 config.Data.publication = True
 config.Data.inputDBS = 'global' #'http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet'
 config.Data.splitting = 'LumiBased'
-config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/miniAOD/data_Jet_Run2015A-PromptReco-v1/'
+config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/miniAOD/data_Commissioning_Run2015A-PromptReco-v1/'
 
 config.section_('User')
 
