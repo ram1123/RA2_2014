a77d08cec3a5da9276a0e9f96ef37e0916ce4f49
lbrianza <lucabrianza@gmail.com>
2015-07-10 14:17:00 +0200

    changes in crab cfg for 50ns production

--- TreeMaker/test/prod_flatTrees_MC74x_50ns.py
+++ TreeMaker/test/prod_flatTrees_MC74x_50ns.py
@@ -24,105 +24,105 @@ if __name__ == '__main__':
     from CRABAPI.RawCommand import crabCommand
 
     #Make sure you set this parameter (here or above in the config it does not matter)
-    config.General.workArea = 'ntuple50ns'
+    config.General.workArea = 'ntuple'
 
     def submit(config):
         res = crabCommand('submit', config = config)
 
     #########    From now on that's what users should modify: this is the a-la-CRAB2 configuration part.
         
-    config.General.requestName = 'WJets'
+    config.General.requestName = 'WJets_50ns'
     config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/WJets/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/WJets_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
 
-    config.General.requestName = 'WW'
+    config.General.requestName = 'WW_50ns'
     config.Data.inputDataset = '/WW_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/WW/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/WW_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
 
 
-    config.General.requestName = 'WZ'
+    config.General.requestName = 'WZ_50ns'
     config.Data.inputDataset = '/WZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/WZ/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/WZ_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
     
 
-    config.General.requestName = 'ZZ'
+    config.General.requestName = 'ZZ_50ns'
     config.Data.inputDataset = '/ZZ_TuneCUETP8M1_13TeV-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/ZZ/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/ZZ_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
     
 
-    config.General.requestName = 'TTbar_amcatnlo'
+    config.General.requestName = 'TTbar_amcatnlo_50ns'
     config.Data.inputDataset = '/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/TTbar_amcatnlo/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/TTbar_amcatnlo_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
 
 
-    config.General.requestName = 'TTbar_powheg'
+    config.General.requestName = 'TTbar_powheg_50ns'
     config.Data.inputDataset = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v3/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/TTbar_powheg/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/TTbar_powheg_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
 
 
-    config.General.requestName = 'TTbar_madgraph'
+    config.General.requestName = 'TTbar_madgraph_50ns'
     config.Data.inputDataset = '/TTJets_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/TTbar_madgraph/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/TTbar_madgraph_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
     
 
-    config.General.requestName = 'tch'
+    config.General.requestName = 'tch_50ns'
     config.Data.inputDataset = '/ST_t-channel_top_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/tch/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/tch_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
 
 
-    config.General.requestName = 'tch_bar'
+    config.General.requestName = 'tch_bar_50ns'
     config.Data.inputDataset = '/ST_t-channel_antitop_4f_leptonDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/tch_bar/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/tch_bar_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
     
 
-    config.General.requestName = 'tW_bar'
+    config.General.requestName = 'tW_bar_50ns'
     config.Data.inputDataset = '/ST_tW_antitop_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v2/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/tWch_bar/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/tWch_bar_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
     
 
-    config.General.requestName = 'tWch'
+    config.General.requestName = 'tWch_50ns'
     config.Data.inputDataset = '/ST_tW_top_5f_inclusiveDecays_13TeV-powheg-pythia8_TuneCUETP8M1/RunIISpring15DR74-Asympt50ns_MCRUN2_74_V9A-v1/MINIAODSIM'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple50ns/tWch/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple/tWch_50ns/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
