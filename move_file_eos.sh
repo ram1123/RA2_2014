73e990e511aad98a4e51569e17ed3a0ad39a1333
lbrianza <lucabrianza@gmail.com>
2015-10-24 11:07:13 +0200

    some changes to the move file script

--- move_file_eos.sh
+++ move_file_eos.sh
@@ -1,73 +1,75 @@
-mv RSGraviton600/*/*/*/*/ReducedSelection_* RSGraviton600/
-mv RSGraviton800/*/*/*/*/ReducedSelection_* RSGraviton800/
-mv RSGraviton1000/*/*/*/*/ReducedSelection_* RSGraviton1000/
-mv RSGraviton1200/*/*/*/*/ReducedSelection_* RSGraviton1200/
-mv RSGraviton1400/*/*/*/*/ReducedSelection_* RSGraviton1400/
-mv RSGraviton1600/*/*/*/*/ReducedSelection_* RSGraviton1600/
-mv RSGraviton1800/*/*/*/*/ReducedSelection_* RSGraviton1800/
-mv RSGraviton2000/*/*/*/*/ReducedSelection_* RSGraviton2000/
-mv RSGraviton2500/*/*/*/*/ReducedSelection_* RSGraviton2500/
-mv RSGraviton3000/*/*/*/*/ReducedSelection_* RSGraviton3000/
-mv RSGraviton3500/*/*/*/*/ReducedSelection_* RSGraviton3500/
-mv RSGraviton4000/*/*/*/*/ReducedSelection_* RSGraviton4000/
-mv RSGraviton4500/*/*/*/*/ReducedSelection_* RSGraviton4500/
-mv BulkGraviton800/*/*/*/*/ReducedSelection_* BulkGraviton800/
-mv BulkGraviton1000/*/*/*/*/ReducedSelection_* BulkGraviton1000/
-mv BulkGraviton1200/*/*/*/*/ReducedSelection_* BulkGraviton1200/
-mv BulkGraviton1400/*/*/*/*/ReducedSelection_* BulkGraviton1400/
-mv BulkGraviton1600/*/*/*/*/ReducedSelection_* BulkGraviton1600/
-mv BulkGraviton1800/*/*/*/*/ReducedSelection_* BulkGraviton1800/
-mv BulkGraviton2000/*/*/*/*/ReducedSelection_* BulkGraviton2000/
-mv BulkGraviton2500/*/*/*/*/ReducedSelection_* BulkGraviton2500/
-mv BulkGraviton3000/*/*/*/*/ReducedSelection_* BulkGraviton3000/
-mv BulkGraviton4000/*/*/*/*/ReducedSelection_* BulkGraviton4000/
-mv BulkGraviton4500/*/*/*/*/ReducedSelection_* BulkGraviton4500/
-mv WprimeToWZ600/*/*/*/*/ReducedSelection_* WprimeToWZ600/
-mv WprimeToWZ800/*/*/*/*/ReducedSelection_* WprimeToWZ800/
-mv WprimeToWZ1000/*/*/*/*/ReducedSelection_* WprimeToWZ1000/
-mv WprimeToWZ1200/*/*/*/*/ReducedSelection_* WprimeToWZ1200/
-mv WprimeToWZ1400/*/*/*/*/ReducedSelection_* WprimeToWZ1400/
-mv WprimeToWZ1600/*/*/*/*/ReducedSelection_* WprimeToWZ1600/
-mv WprimeToWZ1800/*/*/*/*/ReducedSelection_* WprimeToWZ1800/
-mv WprimeToWZ2000/*/*/*/*/ReducedSelection_* WprimeToWZ2000/
-mv WprimeToWZ2500/*/*/*/*/ReducedSelection_* WprimeToWZ2500/
-mv WprimeToWZ3000/*/*/*/*/ReducedSelection_* WprimeToWZ3000/
-mv WprimeToWZ3500/*/*/*/*/ReducedSelection_* WprimeToWZ3500/
-mv WprimeToWZ4000/*/*/*/*/ReducedSelection_* WprimeToWZ4000/
-mv WprimeToWZ4500/*/*/*/*/ReducedSelection_* WprimeToWZ4500/
-mv WJets/*/*/*/*/ReducedSelection_* WJets/
-mv WJets100/*/*/*/*/ReducedSelection_* WJets100/
-mv WJets200/*/*/*/*/ReducedSelection_* WJets200/
-mv WJets400/*/*/*/*/ReducedSelection_* WJets400/
-mv WJets600/*/*/*/*/ReducedSelection_* WJets600/
-mv TTbar_amcatnlo/*/*/*/*/ReducedSelection_* TTbar_amcatnlo/
-mv TTbar_powheg/*/*/*/*/ReducedSelection_* TTbar_powheg/
-mv TTbar_madgraph/*/*/*/*/ReducedSelection_* TTbar_madgraph/
-mv WW/*/*/*/*/ReducedSelection_* WW/
-mv WZ/*/*/*/*/ReducedSelection_* WZ/
-mv ZZ/*/*/*/*/ReducedSelection_* ZZ/
-mv WW_excl/*/*/*/*/ReducedSelection_* WW_excl/
-mv WZ_excl/*/*/*/*/ReducedSelection_* WZ_excl/
-mv ZZ_excl/*/*/*/*/ReducedSelection_* ZZ_excl/
-mv sch/*/*/*/*/ReducedSelection_* sch/
-mv tch/*/*/*/*/ReducedSelection_* tch/
-mv tch_bar/*/*/*/*/ReducedSelection_* tch_bar/
-mv tWch/*/*/*/*/ReducedSelection_* tWch/
-mv tWch_bar/*/*/*/*/ReducedSelection_* tWch_bar/
-mv WJets_50ns/*/*/*/*/ReducedSelection_* WJets_50ns/
-mv TTbar_amcatnlo_50ns/*/*/*/*/ReducedSelection_* TTbar_amcatnlo_50ns/
-mv TTbar_powheg_50ns/*/*/*/*/ReducedSelection_* TTbar_powheg_50ns/
-mv TTbar_madgraph_50ns/*/*/*/*/ReducedSelection_* TTbar_madgraph_50ns/
-mv WW_50ns/*/*/*/*/ReducedSelection_* WW_50ns/
-mv WZ_50ns/*/*/*/*/ReducedSelection_* WZ_50ns/
-mv ZZ_50ns/*/*/*/*/ReducedSelection_* ZZ_50ns/
-mv tch_50ns/*/*/*/*/ReducedSelection_* tch_50ns/
-mv tch_bar_50ns/*/*/*/*/ReducedSelection_* tch_bar_50ns/
-mv tWch_50ns/*/*/*/*/ReducedSelection_* tWch_50ns/
-mv tWch_bar_50ns/*/*/*/*/ReducedSelection_* tWch_bar_50ns/
-mv data_mu_prompt/*/*/*/*/ReducedSelection_* data_mu_prompt/
-mv data_el_prompt/*/*/*/*/ReducedSelection_* data_el_prompt/
-mv data_mu_17jul/*/*/*/*/ReducedSelection_* data_mu_17jul/
-mv data_el_17jul/*/*/*/*/ReducedSelection_* data_el_17jul/
-mv data_mu_prompt_25ns_runC/*/*/*/*/ReducedSelection_* data_mu_prompt_25ns_runC/
-mv data_el_prompt_25ns_runC/*/*/*/*/ReducedSelection_* data_el_prompt_25ns_runC/
+mv -if RSGraviton600/*/*/*/*/ReducedSelection_* RSGraviton600/
+mv -if RSGraviton800/*/*/*/*/ReducedSelection_* RSGraviton800/
+mv -if RSGraviton1000/*/*/*/*/ReducedSelection_* RSGraviton1000/
+mv -if RSGraviton1200/*/*/*/*/ReducedSelection_* RSGraviton1200/
+mv -if RSGraviton1400/*/*/*/*/ReducedSelection_* RSGraviton1400/
+mv -if RSGraviton1600/*/*/*/*/ReducedSelection_* RSGraviton1600/
+mv -if RSGraviton1800/*/*/*/*/ReducedSelection_* RSGraviton1800/
+mv -if RSGraviton2000/*/*/*/*/ReducedSelection_* RSGraviton2000/
+mv -if RSGraviton2500/*/*/*/*/ReducedSelection_* RSGraviton2500/
+mv -if RSGraviton3000/*/*/*/*/ReducedSelection_* RSGraviton3000/
+mv -if RSGraviton3500/*/*/*/*/ReducedSelection_* RSGraviton3500/
+mv -if RSGraviton4000/*/*/*/*/ReducedSelection_* RSGraviton4000/
+mv -if RSGraviton4500/*/*/*/*/ReducedSelection_* RSGraviton4500/
+mv -if BulkGraviton600/*/*/*/*/ReducedSelection_* BulkGraviton600/
+mv -if BulkGraviton800/*/*/*/*/ReducedSelection_* BulkGraviton800/
+mv -if BulkGraviton1000/*/*/*/*/ReducedSelection_* BulkGraviton1000/
+mv -if BulkGraviton1200/*/*/*/*/ReducedSelection_* BulkGraviton1200/
+mv -if BulkGraviton1400/*/*/*/*/ReducedSelection_* BulkGraviton1400/
+mv -if BulkGraviton1600/*/*/*/*/ReducedSelection_* BulkGraviton1600/
+mv -if BulkGraviton1800/*/*/*/*/ReducedSelection_* BulkGraviton1800/
+mv -if BulkGraviton2000/*/*/*/*/ReducedSelection_* BulkGraviton2000/
+mv -if BulkGraviton2500/*/*/*/*/ReducedSelection_* BulkGraviton2500/
+mv -if BulkGraviton3000/*/*/*/*/ReducedSelection_* BulkGraviton3000/
+mv -if BulkGraviton3500/*/*/*/*/ReducedSelection_* BulkGraviton3500/
+mv -if BulkGraviton4000/*/*/*/*/ReducedSelection_* BulkGraviton4000/
+mv -if BulkGraviton4500/*/*/*/*/ReducedSelection_* BulkGraviton4500/
+mv -if WprimeToWZ600/*/*/*/*/ReducedSelection_* WprimeToWZ600/
+mv -if WprimeToWZ800/*/*/*/*/ReducedSelection_* WprimeToWZ800/
+mv -if WprimeToWZ1000/*/*/*/*/ReducedSelection_* WprimeToWZ1000/
+mv -if WprimeToWZ1200/*/*/*/*/ReducedSelection_* WprimeToWZ1200/
+mv -if WprimeToWZ1400/*/*/*/*/ReducedSelection_* WprimeToWZ1400/
+mv -if WprimeToWZ1600/*/*/*/*/ReducedSelection_* WprimeToWZ1600/
+mv -if WprimeToWZ1800/*/*/*/*/ReducedSelection_* WprimeToWZ1800/
+mv -if WprimeToWZ2000/*/*/*/*/ReducedSelection_* WprimeToWZ2000/
+mv -if WprimeToWZ2500/*/*/*/*/ReducedSelection_* WprimeToWZ2500/
+mv -if WprimeToWZ3000/*/*/*/*/ReducedSelection_* WprimeToWZ3000/
+mv -if WprimeToWZ3500/*/*/*/*/ReducedSelection_* WprimeToWZ3500/
+mv -if WprimeToWZ4000/*/*/*/*/ReducedSelection_* WprimeToWZ4000/
+mv -if WprimeToWZ4500/*/*/*/*/ReducedSelection_* WprimeToWZ4500/
+mv -if WJets/*/*/*/*/ReducedSelection_* WJets/
+mv -if WJets_madgraph/*/*/*/*/ReducedSelection_* WJets_madgraph/
+mv -if WJets100/*/*/*/*/ReducedSelection_* WJets100/
+mv -if WJets200/*/*/*/*/ReducedSelection_* WJets200/
+mv -if WJets400/*/*/*/*/ReducedSelection_* WJets400/
+mv -if WJets600/*/*/*/*/ReducedSelection_* WJets600/
+mv -if WJets600bis/*/*/*/*/ReducedSelection_* WJets600bis/
+mv -if WJets800/*/*/*/*/ReducedSelection_* WJets800/
+mv -if WJets1200/*/*/*/*/ReducedSelection_* WJets1200/
+mv -if WJets2500/*/*/*/*/ReducedSelection_* WJets2500/
+mv -if TTbar_amcatnlo/*/*/*/*/ReducedSelection_* TTbar_amcatnlo/
+mv -if TTbar_powheg/*/*/*/*/ReducedSelection_* TTbar_powheg/
+mv -if TTbar_madgraph/*/*/*/*/ReducedSelection_* TTbar_madgraph/
+mv -if TTbar600/*/*/*/*/ReducedSelection_* TTbar600/
+mv -if TTbar800/*/*/*/*/ReducedSelection_* TTbar800/
+mv -if TTbar1200/*/*/*/*/ReducedSelection_* TTbar1200/
+mv -if TTbar2500/*/*/*/*/ReducedSelection_* TTbar2500/
+mv -if WW/*/*/*/*/ReducedSelection_* WW/
+mv -if WZ/*/*/*/*/ReducedSelection_* WZ/
+mv -if ZZ/*/*/*/*/ReducedSelection_* ZZ/
+mv -if WW_excl/*/*/*/*/ReducedSelection_* WW_excl/
+mv -if WZ_excl/*/*/*/*/ReducedSelection_* WZ_excl/
+mv -if ZZ_excl/*/*/*/*/ReducedSelection_* ZZ_excl/
+mv -if sch/*/*/*/*/ReducedSelection_* sch/
+mv -if tch/*/*/*/*/ReducedSelection_* tch/
+mv -if tch_bar/*/*/*/*/ReducedSelection_* tch_bar/
+mv -if tWch/*/*/*/*/ReducedSelection_* tWch/
+mv -if tWch_bar/*/*/*/*/ReducedSelection_* tWch_bar/
+mv -if data_mu_prompt_25ns_runC/*/*/*/*/ReducedSelection_* data_mu_prompt_25ns_runC/
+mv -if data_el_prompt_25ns_runC/*/*/*/*/ReducedSelection_* data_el_prompt_25ns_runC/
+mv -if data_mu_prompt_v3_25ns_runD/*/*/*/*/ReducedSelection_* data_mu_prompt_v3_25ns_runD/
+mv -if data_el_prompt_v3_25ns_runD/*/*/*/*/ReducedSelection_* data_el_prompt_v3_25ns_runD/
+mv -if data_mu_prompt_v4_25ns_runD/*/*/*/*/ReducedSelection_* data_mu_prompt_v4_25ns_runD/
+mv -if data_el_prompt_v4_25ns_runD/*/*/*/*/ReducedSelection_* data_el_prompt_v4_25ns_runD/
+mv -if data_mu_05Oct_25ns_runD/*/*/*/*/ReducedSelection_* data_mu_05Oct_25ns_runD/
+mv -if data_el_05Oct_25ns_runD/*/*/*/*/ReducedSelection_* data_el_05Oct_25ns_runD/
