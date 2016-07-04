bb06307f33d0eabd494ad857410f8ff2fe4fe94a
lbrianza <lucabrianza@gmail.com>
2016-07-04 10:55:18 +0200

    new json file (5.8fb)

--- TreeMaker/test/prod_flatTrees_run2_data_80x.py
+++ TreeMaker/test/prod_flatTrees_run2_data_80x.py
@@ -14,11 +14,13 @@ config.JobType.maxJobRuntimeMin = 900 #15 h
 config.JobType.inputFiles = ['Spring16_25nsV3_DATA_L1FastJet_AK8PFchs.txt','Spring16_25nsV3_DATA_L2Relative_AK8PFchs.txt','Spring16_25nsV3_DATA_L3Absolute_AK8PFchs.txt','Spring16_25nsV3_DATA_L2L3Residual_AK8PFchs.txt','Spring16_25nsV3_DATA_L1FastJet_AK4PFchs.txt','Spring16_25nsV3_DATA_L2Relative_AK4PFchs.txt','Spring16_25nsV3_DATA_L3Absolute_AK4PFchs.txt','Spring16_25nsV3_DATA_L2L3Residual_AK4PFchs.txt','Spring16_25nsV3_DATA_Uncertainty_AK4PFchs.txt','Spring16_25nsV3_DATA_Uncertainty_AK8PFchs.txt','Spring16_25nsV3_DATA_L1FastJet_AK8PFPuppi.txt','Spring16_25nsV3_DATA_L2Relative_AK8PFPuppi.txt','Spring16_25nsV3_DATA_L3Absolute_AK8PFPuppi.txt','Spring16_25nsV3_DATA_L2L3Residual_AK8PFPuppi.txt','Spring16_25nsV3_DATA_L1FastJet_AK4PFPuppi.txt','Spring16_25nsV3_DATA_L2Relative_AK4PFPuppi.txt','Spring16_25nsV3_DATA_L3Absolute_AK4PFPuppi.txt','Spring16_25nsV3_DATA_L2L3Residual_AK4PFPuppi.txt','Spring16_25nsV3_DATA_Uncertainty_AK4PFPuppi.txt','Spring16_25nsV3_DATA_Uncertainty_AK8PFPuppi.txt' ]
 config.section_('Data')
 config.Data.inputDataset = '/SingleMuon/Run2015B-PromptReco-v1/MINIAOD'
-config.Data.unitsPerJob = 10
-config.Data.lumiMask = 'json/Cert_271036-274443_13TeV_PromptReco_Collisions16_JSON.txt' #2016, 2.6/fb
+config.Data.unitsPerJob = 7
+#config.Data.lumiMask = 'json/Cert_271036-275125_13TeV_PromptReco_Collisions16_JSON.txt' #2016, 3.99/fb
+config.Data.lumiMask = 'json/Cert_271036-275783_13TeV_PromptReco_Collisions16_JSON_NoL1T.txt' #5.8/fb
 config.Data.inputDBS = 'global' #'http://cmsdbsprod.cern.ch/cms_dbs_prod_global/servlet/DBSServlet'
 config.Data.splitting = 'LumiBased'
 config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/ntuple_data_mu/'
+#config.Data.runRange = '275126-275783'
 config.section_('User')
 config.section_('Site')
 config.Site.storageSite = 'T2_CH_CERN'
@@ -28,41 +30,58 @@ if __name__ == '__main__':
     from CRABAPI.RawCommand import crabCommand
 
     #Make sure you set this parameter (here or above in the config it does not matter)
-    config.General.workArea = 'data_17jun2016_jecV7_v2'
+    config.General.workArea = 'data_topup_3jul_275126-275783_v2'
 
     def submit(config):
         res = crabCommand('submit', config = config)
 
     #########    From now on that's what users should modify: this is the a-la-CRAB2 configuration part.
-        
+    '''
     config.General.requestName = 'data_el_2016_runB_v1'
     config.Data.inputDataset = '/SingleElectron/Run2016B-PromptReco-v1/MINIAOD'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/data_17jun2016_jecV7_v2/data_el_2016_runB_v1/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/data_topup_3jul_275126-275783_v2/data_el_2016_runB_v1/'
+    from multiprocessing import Process
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
+    '''
+    config.General.requestName = 'data_el_2016_runB_v2'
+    config.Data.inputDataset = '/SingleElectron/Run2016B-PromptReco-v2/MINIAOD'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/data_topup_3jul_275126-275783_v2/data_el_2016_runB_v2/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
 
-    config.General.requestName = 'data_el_2016_runB_v2'
-    config.Data.inputDataset = '/SingleElectron/Run2016B-PromptReco-v2/MINIAOD'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/data_17jun2016_jecV7_v2/data_el_2016_runB_v2/'
+    config.General.requestName = 'data_el_2016_runC_v2'
+    config.Data.inputDataset = '/SingleElectron/Run2016C-PromptReco-v2/MINIAOD'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/data_topup_3jul_275126-275783_v2/data_el_2016_runC_v2/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
     
-
+    '''
     config.General.requestName = 'data_mu_2016_runB_v1'
     config.Data.inputDataset = '/SingleMuon/Run2016B-PromptReco-v1/MINIAOD'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/data_17jun2016_jecV7_v2/data_mu_2016_runB_v1/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/data_topup_3jul_275126-275783_v2/data_mu_2016_runB_v1/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
     p.join()
 
+    '''
     config.General.requestName = 'data_mu_2016_runB_v2'
     config.Data.inputDataset = '/SingleMuon/Run2016B-PromptReco-v2/MINIAOD'
-    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/data_17jun2016_jecV7_v2/data_mu_2016_runB_v2/'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/data_topup_3jul_275126-275783_v2/data_mu_2016_runB_v2/'
+    from multiprocessing import Process
+    p = Process(target=submit, args=(config,))
+    p.start()
+    p.join()
+
+    config.General.requestName = 'data_mu_2016_runC_v2'
+    config.Data.inputDataset = '/SingleMuon/Run2016C-PromptReco-v2/MINIAOD'
+    config.Data.outLFNDirBase = '/store/group/dpg_ecal/alca_ecalcalib/ecalMIBI/lbrianza/data_topup_3jul_275126-275783_v2/data_mu_2016_runC_v2/'
     from multiprocessing import Process
     p = Process(target=submit, args=(config,))
     p.start()
